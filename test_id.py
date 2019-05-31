7ddf694b38268c722399234630a455eaf58979f1
Taylor Cooke <taylorcooke@gmail.com>
2019-05-31 13:56:00 -0400

    putting unique id in beginning

--- test_id.py
+++ test_id.py
@@ -1,21 +1,21 @@
 import csv
 
-# with open('nametest.csv', newline='') as csvfile:
-#     uat_id = ''
-#     unique = 0
-#     reader = csv.DictReader(csvfile)
-#     for row in reader:
-#         unique += 1
-#         unique_id = str(unique)
-#         uat_id = row['fn'] + "_" + row['ln'] + "_" + row['age'] + "_" + unique_id
-        
-#         print(uat_id)
-
 with open('nametest.csv', newline='') as csvfile:
-    reader = csv.reader(csvfile, delimiter=',')
+    uat_id = ''
     unique = 0
+    reader = csv.DictReader(csvfile)
     for row in reader:
         unique += 1
         unique_id = str(unique)
-        row += unique_id
-        print("_".join(row))
+        uat_id = unique_id + "_" + row['fn'] + "_" + row['ln'] + "_" + row['age']
+        
+        print(uat_id)
+
+# with open('nametest.csv', newline='') as csvfile:
+#     reader = csv.reader(csvfile, delimiter=',')
+#     unique = 0
+#     for row in reader:
+#         unique += 1
+#         unique_id = str(unique)
+#         row += unique_id
+#         print("_".join(row))
