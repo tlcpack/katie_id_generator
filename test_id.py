4561b4cb6500f4fe191980d37a0da89c480bef34
Taylor Cooke <taylorcooke@gmail.com>
2019-05-31 10:46:05 -0400

    prints id using join

--- test_id.py
+++ test_id.py
@@ -1,13 +1,21 @@
 import csv
 
+# with open('nametest.csv', newline='') as csvfile:
+#     uat_id = ''
+#     unique = 0
+#     reader = csv.DictReader(csvfile)
+#     for row in reader:
+#         unique += 1
+#         unique_id = str(unique)
+#         uat_id = row['fn'] + "_" + row['ln'] + "_" + row['age'] + "_" + unique_id
+        
+#         print(uat_id)
+
 with open('nametest.csv', newline='') as csvfile:
-    uat_id = ''
+    reader = csv.reader(csvfile, delimiter=',')
     unique = 0
-    reader = csv.DictReader(csvfile)
     for row in reader:
         unique += 1
         unique_id = str(unique)
-        uat_id = row['fn'] + "_" + row['ln'] + "_" + row['age'] + "_" + unique_id
-        
-        print(uat_id)
-
+        row += unique_id
+        print("_".join(row))
