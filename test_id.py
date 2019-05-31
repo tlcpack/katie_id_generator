42c6e54dc091cb7b8410b93cbcf09b73d5781a8e
Taylor Cooke <taylorcooke@gmail.com>
2019-05-31 10:28:39 -0400

    prints one per line

--- test_id.py
+++ test_id.py
@@ -1,11 +1,9 @@
 import csv
 
 with open('nametest.csv', newline='') as csvfile:
-    uat_id = []
+    
     reader = csv.DictReader(csvfile)
     for row in reader:
-        uat_id.append(row['fn'])
-        uat_id.append(row['ln'])
-        uat_id.append(row['age'])
+        print(row['fn'], row['ln'], row['age'])
 
-print(uat_id)
+print(row)
